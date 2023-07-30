a8f65791fb64a0cde950b15ac8ee5ba5998e99e7
nail444f <140378379+nail444f@users.noreply.github.com>
2023-07-30 17:44:55 +0700

    Add files via upload

---
+++ app/Controllers/Crud11.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\MahasiswaModel;
+
+class Crud extends BaseController
+{
+
+    protected $db;
+    public function __construct()
+    {
+        $this->db = new MahasiswaModel();
+    }
+
+    public function index(){
+
+        $all = $this->db->findAll();
+
+        $data = [
+            'mahasiswa' => $all
+        ];
+        return view('crud/view', $data);
+    }
+
+    public function tambah(){
+        if (isset($_POST['nim'])&& isset($_POST['nama'])) {
+
+            // Menampung data input yang dikirim upload.php
+            // ke variabel $nim
+            $nim = $_POST['nim'];
+            $nama = $_POST['nama'];
+
+            $upload = [
+                'nim' => $nim,
+                'nama' => $nama
+            ];
+
+            $this->db->insert($upload);
+
+            return redirect()->to(base_url('/crud'));
+        }else{
+            return view('crud/upload');
+        }
+    }
+
+    public function edit($id){
+        $nim = $id;
+        $a = $this->db->find($nim);
+        $data = [
+            'edit' => $a
+        ];
+        return view('crud/edit', $data);
+    }
+
+    public function editan(){
+        $nim = $_POST['nim'];
+        $newNim = $_POST['newNim'];
+        $nama = $_POST['nama'];
+        $newNama = $_POST['newNama'];
+        
+        // Mengatur data yang akan diupdate
+        $data = [];
+
+        if (!empty($newNim)) {
+            $data['nim'] = $newNim;
+        }
+
+        if (!empty($newNama)) {
+            $data['nama'] = $newNama;
+        }
+
+        // Melakukan update jika ada perubahan data
+        if (!empty($data)) {
+            $this->db->where('nim', $nim)
+                ->update($data);
+        }
+        return redirect()->to(base_url('/crud'));
+    }
+
+    public function hapus($id){
+        $nim = $id;
+        $this->db->delete($nim);
+        return redirect()->to("/crud");
+    }
+}
---
+++ app/Views/navbar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge" /> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>My Navbar</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
+  <body>
+    <nav>
+      <div class="logo">
+        <h4>Cholid Fajar</h4>
+      </div>
+
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Product</a></li>
+        <li><a href="#">Service</a></li>
+        <li><a href="#">Galeri</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+
+      <div class="menu-toggle">
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
+
+    <script src="assets/js/script.js"></script>
+  </body>
+</html>
